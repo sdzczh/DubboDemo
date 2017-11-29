d049cef0ec21138c7f03b7c0519925adc1a773b2
sdzczh <zgsdzczh@163.com>
2017-11-29 15:21:56 +0800

    zookeeper+Dubbo环境搭建及简单Demo 运行环境 本文运行环境：windows 7  + JDK1.8 + zookeeper-3.3.6 + dubbo-admin-2.5.4 （支持JDK 1.8）+ dubbo 2.5.3

---
+++ dubbo-provider/src/main/java/com/zh/dubbo/demo/impl/DemoServiceImpl.java
@@ -0,0 +1,16 @@
+package com.zh.dubbo.demo.impl;
+
+import com.zh.dubbo.demo.DemoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoServiceImpl implements DemoService {
+    public List<String> getPermissions(Long id) {
+        List<String> demo = new ArrayList<String>();
+        demo.add(String.format("Permission_%d", id - 1));
+        demo.add(String.format("Permission_%d", id));
+        demo.add(String.format("Permission_%d", id + 1));
+        return demo;
+    }
+}
